004d544a17f5a84cdd4845cb22f947c63af8b93a
habeeb_mohammed <habeeb_mohammed@icloud-ready.co>
2025-09-05 15:44:48 +0300

    feat : add user routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,15 @@
 import express from "express";
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
+import cors from "cors";
+import { clerkMiddleware } from "@clerk/express";
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+// app.use(express.urlencoded({ extended: true }))
+app.use("/api/users", userRoutes);
+app.use(clerkMiddleware());
 
 app.get("/", (req, res) => {
   res.send("Hello from server");
@@ -14,7 +21,9 @@ const startServer = async () => {
 
     // listen for local development
     if (ENV.NODE_ENV !== "production") {
-      app.listen(ENV.PORT, () => console.log("Server is up and running on PORT:", ENV.PORT));
+      app.listen(ENV.PORT, () =>
+        console.log("Server is up and running on PORT:", ENV.PORT)
+      );
     }
   } catch (error) {
     console.error("Failed to start server:", error.message);
@@ -23,6 +32,3 @@ const startServer = async () => {
 };
 
 startServer();
-
-
-
