a714afb85b53c197d42c52a686d4ae81393093c1
habeeb_mohammed <habeeb_mohammed@icloud-ready.co>
2025-09-10 12:31:26 +0300

    Feat : add tabs setup with signout

---
+++ mobile/app/(tabs)/_layout.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+import { Tabs, Redirect } from "expo-router";
+import { Feather } from "@expo/vector-icons";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { useAuth } from "@clerk/clerk-expo";
+
+const TabsLayout = () => {
+  const insets = useSafeAreaInsets();
+  const { isSignedIn } = useAuth();
+  if (!isSignedIn) return <Redirect href="/(auth)" />;
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "#1DA1F2",
+        tabBarInactiveTintColor: "#657786",
+        tabBarStyle: {
+          backgroundColor: "#fff",
+          borderTopWidth: 1,
+          borderTopColor: "#E1E8ED",
+          height: 50 + insets.bottom,
+          paddingTop: 8,
+        },
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: "Search",
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="search" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="notifications"
+        options={{
+          title: "Notifications",
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="bell" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="messages"
+        options={{
+          title: "Messages",
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="message-circle" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="user" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+};
+
+export default TabsLayout;
