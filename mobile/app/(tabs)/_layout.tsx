69534adde4efc25a7b17e1b42c29c2a0ba3426cd
habeeb_mohammed <habeeb_mohammed@icloud-ready.co>
2025-09-11 12:40:15 +0300

    Feat:inital search screen

--- mobile/app/(tabs)/_layout.tsx
+++ mobile/app/(tabs)/_layout.tsx
@@ -27,7 +27,7 @@ const TabsLayout = () => {
       <Tabs.Screen
         name="index"
         options={{
-          title: "Home",
+          title: "",
           headerShown: false,
           tabBarIcon: ({ color, size }) => (
             <Feather name="home" color={color} size={size} />
@@ -37,7 +37,7 @@ const TabsLayout = () => {
       <Tabs.Screen
         name="search"
         options={{
-          title: "Search",
+          title: "",
           headerShown: false,
           tabBarIcon: ({ color, size }) => (
             <Feather name="search" color={color} size={size} />
@@ -47,7 +47,7 @@ const TabsLayout = () => {
       <Tabs.Screen
         name="notifications"
         options={{
-          title: "Notifications",
+          title: "",
           headerShown: false,
           tabBarIcon: ({ color, size }) => (
             <Feather name="bell" color={color} size={size} />
@@ -67,7 +67,7 @@ const TabsLayout = () => {
       <Tabs.Screen
         name="profile"
         options={{
-          title: "Profile",
+          title: "",
           headerShown: false,
           tabBarIcon: ({ color, size }) => (
             <Feather name="user" color={color} size={size} />
